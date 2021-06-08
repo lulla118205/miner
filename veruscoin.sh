4b96631d6f34d5cab7a1312eec2ff432e1e5b6eb
lulla118205 <85560085+lulla118205@users.noreply.github.com>
2021-06-08 19:19:39 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+#veruscoin
+apt update
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+./ccminer  -a verus  -o stratum+tcp://ap.luckpool.net:3956  -u  RBNh7KwNNaHbJVUPZXAetaoYim429P2wHj.m.lulla irawan  -p x  -t 6
